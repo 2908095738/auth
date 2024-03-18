a815f632796871cdb3e7d21d89869f8969d79274
luchenlin <2908095738@qq.com>
2024-03-18 21:42:27 +0800

    feat: zookeeper

---
+++ auth/src/main/java/com/auth/conf/zookeeper/ZookeeperConf.java
@@ -0,0 +1,55 @@
+package com.auth.conf.zookeeper;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * Zookeeper Config
+ * @see <a href=https://blog.csdn.net/qq_51513626/article/details/135534442>参考</a>
+ */
+@Configuration
+public class ZookeeperConf {
+
+    private static final long serialVersionUID = -9025878246972668136L;
+
+    // 服务器连接地址，集群模式则使用逗号分隔如：ip1:host,ip2:host
+    private String connectUrl;
+
+    // 会话超时时间：单位ms
+    private Integer sessionTimeout;
+
+    // 连接超时时间：单位ms
+    private Integer connectionTimeout;
+
+    // ACL权限控制，验证策略
+    private String scheme;
+
+    // 验证内容id
+    private String authId;
+
+    @Resource
+    private CuratorRetryPolicy curatorRetryPolicy;
+
+    @Bean
+    public CuratorFramework curatorFramework(){
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString(connectUrl)
+                .sessionTimeoutMs(sessionTimeout)
+                .connectionTimeoutMs(connectionTimeout)
+                // 权限认证
+                //.authorization(scheme,authId.getBytes(StandardCharsets.UTF_8))
+                // 重试策略
+                .retryPolicy(new ExponentialBackoffRetry(curatorRetryPolicy.getBaseSleepTime()
+                        ,curatorRetryPolicy.getMaxRetries()
+                        ,curatorRetryPolicy.getMaxSleep()))
+                .build();
+        // 启动客户端
+        curatorFramework.start();
+        return curatorFramework;
+    }
+}
