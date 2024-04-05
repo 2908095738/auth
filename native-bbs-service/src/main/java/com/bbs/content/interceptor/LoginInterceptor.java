475bf3707dc43b1fa9261e369b53415dc1849426
weimengyao <weimengyao@piesat.cn>
2024-04-05 22:33:53 +0800

    feat:提交获取用户信息

--- native-bbs-service/src/main/java/com/bbs/content/interceptor/LoginInterceptor.java
+++ native-bbs-service/src/main/java/com/bbs/content/interceptor/LoginInterceptor.java
@@ -1,7 +1,8 @@
 package com.bbs.content.interceptor;
 
 
-import com.bbs.content.util.TokenUtil;
+import com.bbs.content.util.AuthUtil;
+import com.bbs.content.util.ThreadLocalUtil;
 import com.bbs.exception.ReLoginException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,38 +14,40 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
 import java.io.IOException;
 
+import static com.alibaba.fastjson.JSON.toJSONString;
+import static java.util.Objects.nonNull;
+
 @Component
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
     @Resource
-    private TokenUtil tokenUtil;
+    private AuthUtil.UserAPI api;
 
 
     @Override
     public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws ReLoginException, IOException {
         String token = request.getHeader("Authorization");
         if (token != null) {
-            if(tokenUtil.verifyToken(token))return true;
-//            if (nonNull(userVO)){
-//                log.debug("[LoginInterceptor::afterCompletion] 用户信息：{}", toJSONString(userVO));
-//                ThreadLocalUtil.addCurrentUser(userVO);
-//                //把获取到的token放到Header里
-//                response.setHeader("token", tokenName);
-//                return true;
-//            }
+            AuthUtil.UserAPI.User user = api.getLoginUser();
+            if (nonNull(user)){
+                log.debug("[LoginInterceptor::afterCompletion] 用户信息：{}", toJSONString(user));
+                ThreadLocalUtil.addCurrentUser(user);
+                return true;
+            }
         }
         response.sendError(401,"请重新登陆");
         return false;
     }
 
 
-//    /**
-//     * 接口访问结束后，从ThreadLocal中删除用户信息
-//     */
-//    @Override
-//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-//        log.info("[LoginInterceptor::afterCompletion] 业务处理结束，开始回收资源：ThreadLocal...");
-//        ThreadLocalUtil.remove();
-//    }
+    /**
+     * 接口访问结束后，从ThreadLocal中删除用户信息
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("[LoginInterceptor::afterCompletion] 业务处理结束，开始回收资源：ThreadLocal...");
+        ThreadLocalUtil.remove();
+    }
+
 
 }
