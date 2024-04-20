3a329d72e93bc90806160e32b1bdf72aa097a8fd
luchenlin <2908095738@qq.com>
2024-04-20 16:04:24 +0800

    fix: bug fix

--- chat/src/main/java/com/bbs/chat/app/chat/api/ChatList.java
+++ chat/src/main/java/com/bbs/chat/app/chat/api/ChatList.java
@@ -3,21 +3,10 @@ package com.bbs.chat.app.chat.api;
 import com.bbs.Result;
 import com.bbs.api.Auth;
 import com.bbs.chat.app.chat.cache.ChatListCache;
-import com.bbs.chat.app.chat.queue.UserMessageQueue;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
-
-import static java.util.Objects.nonNull;
-import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
 
 @Slf4j
 @RestController
@@ -30,15 +19,9 @@ public class ChatList {
     private Auth.UserAPI api;
 
     @GetMapping("/chats")
-    public Result<List<ChatListCache.Chat>> getList() {
+    public Result<ChatListCache.VO> getList() {
         Auth.UserAPI.User loginUser = api.getLoginUser();
         return Result.success(chatListCache.getChatList(loginUser.getId()));
-    }
-
-    @GetMapping("/chats/unread/size")
-    public Result<Integer> getUnreadMessageSize() {
-        Auth.UserAPI.User loginUser = api.getLoginUser();
-        return Result.success(chatListCache.getUnreadMessageSize(loginUser.getId()));
     }
 
     @DeleteMapping("/chats/unread/{id}")
