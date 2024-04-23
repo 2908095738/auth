7d1f544a7eda878087b8b72559d90ca319af94c4
weimengyao <weimengyao@piesat.cn>
2024-04-23 18:39:03 +0800

    fix:添加日志

--- dfs-server/src/main/java/com/bbs/file/controller/Upload.java
+++ dfs-server/src/main/java/com/bbs/file/controller/Upload.java
@@ -2,11 +2,12 @@ package com.bbs.file.controller;
 
 import com.bbs.Result;
 import com.bbs.file.util.minio.FileOpt;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-
+@Slf4j
 @RestController
 @RequestMapping
 public class Upload {
@@ -22,8 +23,14 @@ public class Upload {
             @RequestParam Integer fileType,
             @RequestParam("file") MultipartFile file,
             @RequestParam String contentType
-    ) throws IllegalArgumentException {
+    ) {
+        try {
         String resourceID = fileOpt.resourceID(businessCode, resourceType, fileType);
         return Result.success(fileOpt.upload(resourceID, file, contentType));
+    } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        e.printStackTrace();
+        return null;
+    }
     }
 }
