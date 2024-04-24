bffde51df0052319de7dfc5ffd190d808ea94966
luchenlin <2908095738@qq.com>
2024-04-24 20:56:59 +0800

    feat: 城市查询接口，增加本地缓存

--- auth/src/main/java/com/bbs/auth/Auth.java
+++ auth/src/main/java/com/bbs/auth/Auth.java
@@ -6,11 +6,15 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Slf4j
 @MapperScan({"com.bbs.auth.mapper"})
+@EnableAsync
 @EnableRetry    //启用操作重试
+@EnableCaching
 @SpringBootApplication
 public class Auth implements ApplicationRunner {
     public static void main(String[] args) {
