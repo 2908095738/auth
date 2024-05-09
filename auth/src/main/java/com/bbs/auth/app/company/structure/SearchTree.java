e5fe62c5d9254558a366a5f2aff71eb17805f21d
weimengyao <weimengyao@piesat.cn>
2024-05-09 21:38:38 +0800

    fix:去掉缓存

--- auth/src/main/java/com/bbs/auth/app/company/structure/SearchTree.java
+++ auth/src/main/java/com/bbs/auth/app/company/structure/SearchTree.java
@@ -9,14 +9,12 @@ import com.bbs.auth.service.CompanyService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
 import java.util.List;
 
 import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
@@ -31,7 +29,7 @@ public class SearchTree {
 
 
     @GetMapping("/company/structure/tree")
-    @Cacheable("companyStructure")
+//    @Cacheable("companyStructure")
     public Result<VO> search(@RequestParam(name = "id") Long id) {
         List<CompanyStructure> companyStructureList = companyService.searchStructure(id);
         if(companyStructureList.size() == INTEGER_ZERO) companyStructureList = companyService.searchStructure();
