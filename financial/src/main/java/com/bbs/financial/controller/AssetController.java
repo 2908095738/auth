e246b464813cb94a5771ee2f69ca64ef5af8c9f2
weimengyao <weimengyao@piesat.cn>
2024-06-08 17:04:43 +0800

    feat:修改

--- financial/src/main/java/com/bbs/financial/controller/AssetController.java
+++ financial/src/main/java/com/bbs/financial/controller/AssetController.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bbs.Result;
+import com.bbs.api.auth.User;
 import com.bbs.api.auth.UserAPI;
 import com.bbs.api.auth.company.CompanyAPI;
 import com.bbs.financial.entity.Asset;
@@ -30,7 +31,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.bbs.Result.success;
