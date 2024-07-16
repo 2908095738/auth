17cdf2b865fdcd8a4c591028e7a9ddacc05e87d9
luchenlin <2908095738@qq.com>
2024-07-16 17:35:04 +0800

    fix: 删除无用代码

--- financial/src/main/java/com/bbs/financial/service/impl/CertificateAbstractServiceImpl.java
+++ financial/src/main/java/com/bbs/financial/service/impl/CertificateAbstractServiceImpl.java
@@ -18,7 +18,6 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
