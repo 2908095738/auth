bf17038ce5c91ac771959bd6a833c0fdd3044cf3
weimengyao <weimengyao@piesat.cn>
2024-07-24 22:24:51 +0800

    fix:微信公众号扫码关注

--- auth/src/main/java/com/bbs/auth/service/impl/WeiXinLoginServiceImpl.java
+++ auth/src/main/java/com/bbs/auth/service/impl/WeiXinLoginServiceImpl.java
@@ -106,7 +106,7 @@ public class WeiXinLoginServiceImpl implements WeiXinLoginService {
 
     @Override
     public String receive(String signature, String timestamp, String nonce, String echostr, HttpServletRequest request) throws IOException {
-        String xml = "";
+        String xml = "success";
         log.info("微信回调方法开始执行");
         // 验证微信签名
         if (!wxUtil.checkSignature(signature, timestamp, nonce, token)){
@@ -122,40 +122,50 @@ public class WeiXinLoginServiceImpl implements WeiXinLoginService {
             Map<String, String> resXml = wxUtil.ResponseXmlToMap(xmlString);
             String ticket = resXml.get("Ticket"); // 获取二维码凭证
             String fromUserName = resXml.get("FromUserName"); // 获取OpenId
-            String event = resXml.get("Event");
-            switch (event){
-                case "subscribe": //扫描带参数二维码事件-未关注
-                    if(StrUtil.isEmpty(ticket)){
-                        return "";
-                    }
-                    // 处理绑定微信号事件
-                    if ("1".equals(redisUtil.get("WX:"+ticket))){
-                        //先删除
-                        redisUtil.delete("WX:"+ticket);
-                        redisUtil.set("WX:"+ticket, fromUserName,100000L);
-                    }
-                    xml ="<xml>\n" +
-                            "  <ToUserName><![CDATA[" + fromUserName + "]]></ToUserName>\n" +
-                            "  <CreateTime>" + System.currentTimeMillis() + "</CreateTime>\n" +
-                            "  <MsgType><![CDATA[关注成功]]></MsgType>\n" +
-                            "  <ArticleCount>2</ArticleCount>\n" +
-                            "</xml>\n";
-                    break;
-                case "unsubscribe": //取消关注
+            String toUserName = resXml.get("ToUserName");//开发者微信号
+            String msgType = resXml.get("MsgType");//消息类型，event
+            if (msgType.equals("event")) {
+                String event = resXml.get("Event");
+                switch (event){
+                    case "subscribe": //扫描带参数二维码事件-未关注
+                        if(StrUtil.isEmpty(ticket)){
+                            return xml;
+                        }
+                        // 处理绑定微信号事件
+                        if ("1".equals(redisUtil.get("WX:"+ticket))){
+                            //先删除
+                            redisUtil.delete("WX:"+ticket);
+                            redisUtil.set("WX:"+ticket, fromUserName,100000L);
+                        }
+                        xml ="<xml>\n" +
+                                "  <ToUserName><![CDATA[" + fromUserName + "]]></ToUserName>\n" +
+                                "  <FromUserName><![CDATA[" + toUserName + "]]></FromUserName>\n" +
+                                "  <CreateTime> "+ System.currentTimeMillis() + "</CreateTime>\n" +
+                                "  <MsgType><![CDATA[text]]></MsgType>\n" +
+                                "  <Content><![CDATA[科技改变生活！关注成功]]></Content>\n" +
+                                "</xml>>\n";
+                        break;
+                    case "unsubscribe": //取消关注
 
-                    break;
-                case "SCAN":// 扫描带参数二维码事件-已关注
-                    xml ="<xml>\n" +
-                            "  <ToUserName><![CDATA[" + fromUserName + "]]></ToUserName>\n" +
-                            "  <CreateTime>" + System.currentTimeMillis() + "</CreateTime>\n" +
-                            "  <MsgType><![CDATA[扫码成功]]></MsgType>\n" +
-                            "  <ArticleCount>2</ArticleCount>\n" +
-                            "</xml>\n";
-                    break;
-                case "CLICK":   //自定义菜单事件
+                        break;
+                    case "SCAN":// 扫描带参数二维码事件-已关注
+                        xml ="<xml>\n" +
+                                "  <ToUserName><![CDATA[" + fromUserName + "]]></ToUserName>\n" +
+                                "  <FromUserName><![CDATA[" + toUserName + "]]></FromUserName>\n" +
+                                "  <CreateTime>" + System.currentTimeMillis() + "</CreateTime>\n" +
+                                "  <MsgType><![CDATA[text]]></MsgType>\n" +
+                                "  <Content><![CDATA[扫码成功!]]></Content>\n" +
+                                "</xml>\n";
+                        break;
+                    case "CLICK":   //自定义菜单事件
 
-                default:
-                    break;
+                    default:
+                        break;
+                }
+            }else if (msgType.equals("text")) {
+                String Content = resXml.get("Content");//文本消息内容
+                String MsgId = resXml.get("MsgId");//消息id，64位整型
+                log.debug(Content);
             }
         }catch (Exception e){
             e.printStackTrace();
