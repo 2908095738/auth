a6edef6ffe2eb9dbdd86b0bb734f933b65dd20b1
weimengyao <weimengyao@piesat.cn>
2024-07-26 11:39:58 +0800

    fix:添加日志

--- auth/src/main/java/com/bbs/auth/conf/WebConfig.java
+++ auth/src/main/java/com/bbs/auth/conf/WebConfig.java
@@ -32,7 +32,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(createLoginInterceptor())
-                .excludePathPatterns("/**");
+                .addPathPatterns("/**");
     }
 }
 
