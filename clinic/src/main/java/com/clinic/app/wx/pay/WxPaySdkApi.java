42cd097a0c2905793f15ac735412dd41ee637629
weimengyao <weimengyao@piesat.cn>
2024-07-27 16:26:15 +0800

    fix:支付回调bug

--- clinic/src/main/java/com/clinic/app/wx/pay/WxPaySdkApi.java
+++ clinic/src/main/java/com/clinic/app/wx/pay/WxPaySdkApi.java
@@ -35,7 +35,7 @@ public class WxPaySdkApi {
         // 初始化服务
         service = new NativePayService.Builder().config(config.getWxMlConfig()).build();
         try {
-            String orderId = "tradeNo_"+System.currentTimeMillis()/1000+"_"+config.merchantId;
+            String orderId = "tradeNo"+System.currentTimeMillis()/1000+config.merchantId;
             PrepayResponse prepay = prepay(orderId);
             Map<String, Object> resultMap = new HashMap<>();
             resultMap.put("orderId", orderId);
