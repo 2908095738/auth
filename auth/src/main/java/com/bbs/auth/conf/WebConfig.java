b2b16eab9282cedb74393d84cf22bde7c61677db
weimengyao <weimengyao@piesat.cn>
2024-08-08 20:19:06 +0800

    feat:auth添加拦截

---
+++ auth/src/main/java/com/bbs/auth/conf/WebConfig.java
@@ -0,0 +1,57 @@
+package com.bbs.auth.conf;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+
+
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE","GET","POST")
+                .allowedHeaders("*")
+                .exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options")
+                .allowCredentials(false).maxAge(3600);
+    }
+
+    @Bean
+    public LoginInterceptor createLoginInterceptor() {
+        return new LoginInterceptor();
+    }
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(createLoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/test"+ "/**")
+                .excludePathPatterns("/admin/content"+ "/**")
+                .excludePathPatterns("/content/query"+ "/**")
+                .excludePathPatterns("/salary/temp/export"+ "/**")
+                .excludePathPatterns("/weixin/pay/notification"+ "/**")
+                .excludePathPatterns("/weixin/pay/notification"+ "/**")
+                .excludePathPatterns("/user" + "/**")//注册
+                .excludePathPatterns("/login" + "/**");
+    }
+}
+
