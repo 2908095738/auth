96ed59754b9732ef621738bbde24b03eb108cd23
luchenlin <2908095738@qq.com>
2024-08-10 13:04:37 +0800

    fix: bug fix

--- auth/src/main/java/com/bbs/auth/app/user/router/SearchSystemRouter.java
+++ auth/src/main/java/com/bbs/auth/app/user/router/SearchSystemRouter.java
@@ -14,6 +14,7 @@ import com.google.common.base.Preconditions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,14 +52,14 @@ public class SearchSystemRouter {
         private List<Tree<Long>> tree;
     }
 
-//    @Cacheable(cacheNames = "system::router::user")
+    @Cacheable(cacheNames = "system::router::user")
     @GetMapping("/system/router/user")
     public Result<VO> search(@RequestParam String systemCode) {
         Long uid = userService.loginUser().getId();
         System system = systemService.searchBySystemCode(systemCode);
         Long systemId = system.getId();
-        Long companyId = searchCompanyId(uid);
-        tryCreateSystemCompany(systemId, companyId);
+//        Long companyId = searchCompanyId(uid);
+//        tryCreateSystemCompany(systemId, companyId);
         List<SystemRouter> systemRouters = searchRouter(systemId);
         return Result.success(new VO(systemRouters, systemRouterService.toTree(systemRouters)));
     }
