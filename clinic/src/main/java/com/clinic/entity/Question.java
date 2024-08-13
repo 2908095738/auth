d8939c7c3ccf28e8d8579a2b7ecb7aba12dc107f
weimengyao <weimengyao@piesat.cn>
2024-08-13 17:07:23 +0800

    feat:查询论坛

--- clinic/src/main/java/com/clinic/entity/Question.java
+++ clinic/src/main/java/com/clinic/entity/Question.java
@@ -1,11 +1,9 @@
 package com.clinic.entity;
 
-import com.baomidou.mybatisplus.annotation.TableField;
+import cn.hutool.json.JSONArray;
 import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
 /**
  * 
@@ -13,132 +11,48 @@ import java.util.List;
  */
 @Data
 public class Question implements Serializable {
+
     /**
      * 
      */
     private Long id;
 
     /**
-     * 
-     */
-    private Date createdAt;
-
-    /**
-     * 
-     */
-    private Date updatedAt;
-
-    /**
-     * 
-     */
-    private Long userId;
-
-    /**
-     * 
-     */
-    private String inviteUserId;
-
-    /**
-     * 
-     */
-    private Long lastEditUserId;
-
-    /**
-     * 
+     *
      */
     private String title;
 
     /**
      * 
      */
-    private String originalText;
+    private Integer created_at;
+
+
+    /**
+     *
+     */
+    private Integer vote_count;
 
     /**
      * 
      */
-    private String parsedText;
+    private Integer answer_count;
 
     /**
-     * 
+     *
      */
-    private Integer pin;
+    private JSONArray user_info;
 
     /**
-     * 
+     *
      */
-    private Integer show;
+    private JSONArray tags;
 
     /**
-     * 
+     *
      */
-    private Integer status;
+    private String status;
 
-    /**
-     * 
-     */
-    private Integer viewCount;
 
-    /**
-     * 
-     */
-    private Integer uniqueViewCount;
-
-    /**
-     * 
-     */
-    @TableField(value = "vote_count")
-    private Integer voteCount;
-
-    /**
-     * 
-     */
-    @TableField(value = "answer_count")
-    private Integer answerCount;
-
-    /**
-     * 
-     */
-    @TableField(value = "hot_score")
-    private Integer hotScore;
-
-    /**
-     * 
-     */
-    @TableField(value = "collection_count")
-    private Integer collectionCount;
-
-    /**
-     * 
-     */
-    @TableField(value = "follow_count")
-    private Integer followCount;
-
-    /**
-     * 
-     */
-    @TableField(value = "accepted_answer_id")
-    private Long acceptedAnswerId;
-
-    /**
-     * 
-     */
-    @TableField(value = "last_answer_id")
-    private Long lastAnswerId;
-
-    /**
-     * 
-     */
-    @TableField(value = "post_update_time")
-    private Date postUpdateTime;
-
-    /**
-     * 
-     */
-    @TableField(value = "revision_id")
-    private Long revisionId;
-
-    private List<Answer> answers;
-
-    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 }
