5f16c608f41b2c34f8c163c635c77a3a4ec3d8b3
luchenlin <2908095738@qq.com>
2024-08-14 20:34:41 +0800

    fix: Dubbo 缓存冲突

--- auth/src/main/java/com/bbs/auth/Auth.java
+++ auth/src/main/java/com/bbs/auth/Auth.java
@@ -9,11 +9,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
+import java.io.File;
 
 @Slf4j
 @MapperScan({"com.bbs.auth.mapper"})
@@ -25,11 +25,20 @@ import javax.annotation.Resource;
 @EnableDiscoveryClient  //Nacos 服务发现
 public class Auth implements ApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(Auth.class, args);
+        SpringApplication application = new SpringApplication(Auth.class);
+        application.addInitializers(context -> {
+            Environment env = context.getEnvironment();
+            String appName = env.getProperty("spring.application.name");
+            String filePath = System.getProperty("user.home") + File.separator + ".dubbo" +File.separator + appName;
+            System.setProperty("dubbo.meta.cache.filePath", filePath);
+            System.setProperty("dubbo.mapping.cache.filePath",filePath);
+        });
+        application.run(args);
     }
 
     @Override
     public void run(ApplicationArguments args) {
         log.info("。。。。。。。。。。。。。容器初始化完毕。。。。。。。。。。。。。。");
     }
+
 }
