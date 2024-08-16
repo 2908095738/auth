7ba7b7cb615211a40828d6323301140f76c2bde3
weimengyao <weimengyao@piesat.cn>
2024-08-16 18:51:47 +0800

    fix:修改财务系统启动

--- financial/src/main/java/com/bbs/financial/Financial.java
+++ financial/src/main/java/com/bbs/financial/Financial.java
@@ -1,15 +1,22 @@
 package com.bbs.financial;
 
 import cn.hutool.extra.spring.EnableSpringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.io.File;
+
+@Slf4j
 @MapperScan({"com.bbs.financial.mapper"})
 @EnableAsync
 @EnableSpringUtil
@@ -18,8 +25,22 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableDubbo
 @EnableDiscoveryClient  //Nacos 服务发现
-public class Financial {
+public class Financial  implements ApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(Financial.class, args);
+        SpringApplication application = new SpringApplication(Financial.class);
+        application.addInitializers(context -> {
+            Environment env = context.getEnvironment();
+            String appName = env.getProperty("spring.application.name");
+            String filePath = System.getProperty("user.home") + File.separator + ".dubbo" +File.separator + appName;
+            System.setProperty("dubbo.meta.cache.filePath", filePath);
+            System.setProperty("dubbo.mapping.cache.filePath",filePath);
+        });
+        application.run(args);
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) {
+        log.info("。。。。。。。。。。。。。容器初始化完毕。。。。。。。。。。。。。。");
     }
 }
