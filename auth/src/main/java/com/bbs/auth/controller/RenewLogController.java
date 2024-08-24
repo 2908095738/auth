1e320ad75671289e3ad6b70787551a24faaf6b8e
weimengyao <weimengyao@piesat.cn>
2024-08-24 16:35:57 +0800

    fix:修改支付

--- auth/src/main/java/com/bbs/auth/controller/RenewLogController.java
+++ auth/src/main/java/com/bbs/auth/controller/RenewLogController.java
@@ -1,14 +1,13 @@
 package com.bbs.auth.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bbs.Result;
 import com.bbs.auth.entity.RenewLog;
 import com.bbs.auth.service.RenewLogService;
-import com.bbs.auth.util.LoginUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 public class RenewLogController {
@@ -16,9 +15,9 @@ public class RenewLogController {
     @Resource
     private RenewLogService renewLogService;
 
-    @GetMapping("/renew/log")
-    public Result<List<RenewLog>> get() {
-        return Result.success(renewLogService.getListByUserId(LoginUser.getId()));
+    @GetMapping("by/renew/log")
+    public Result<Page<RenewLog>> get(Integer current, Integer size) {
+        return Result.success(renewLogService.page(new Page<>(current,size)));
     }
 
 }
