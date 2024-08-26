3ed9123c463b85cc06608c09afe6b0e5e9e3694d
weimengyao <weimengyao@piesat.cn>
2024-08-26 18:22:27 +0800

    fix：微信登录取消拦截

--- auth/src/main/java/com/bbs/auth/conf/WebConfig.java
+++ auth/src/main/java/com/bbs/auth/conf/WebConfig.java
@@ -49,9 +49,9 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/admin/content"+ "/**")
                 .excludePathPatterns("/content/query"+ "/**")
                 .excludePathPatterns("/salary/temp/export"+ "/**")
-                .excludePathPatterns("/weixin/receive")
-                .excludePathPatterns("/weixin/getQRCode")
-                .excludePathPatterns("/weixin/check")
+                .excludePathPatterns("/weixin/receive"+ "/**")
+                .excludePathPatterns("/weixin/getQRCode"+ "/**")
+                .excludePathPatterns("/weixin/check"+ "/**")
                 .excludePathPatterns("/phone/**")
                 .excludePathPatterns("/user" + "/**")//注册
                 .excludePathPatterns("/login/**")
