2724f189ba1196116829ee066d48556350778bf9
luchenlin <2908095738@qq.com>
2024-08-26 12:29:43 +0800

    feat: 修改【查询支付记录】接口路径 & 新增 druid 监控

--- auth/src/main/java/com/bbs/auth/controller/RenewLogController.java
+++ auth/src/main/java/com/bbs/auth/controller/RenewLogController.java
@@ -4,20 +4,35 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bbs.Result;
 import com.bbs.auth.entity.RenewLog;
 import com.bbs.auth.service.RenewLogService;
+import com.bbs.auth.service.UserService;
+import com.bbs.exception.ReLoginException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+import static java.util.Objects.nonNull;
+
+/**
+ * 支付记录
+ */
 @RestController
 public class RenewLogController {
 
     @Resource
     private RenewLogService renewLogService;
 
-    @GetMapping("/by/renew/log")
-    public Result<Page<RenewLog>> get(Integer current, Integer size) {
-        return Result.success(renewLogService.page(new Page<>(current,size)));
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/back/renew/log")
+    public Result<Page<RenewLog>> get(@RequestParam(required = false) Long userId, Integer current, Integer size) throws IllegalArgumentException, ReLoginException {
+        userService.loginUserIsAdmin();
+        return Result.success(renewLogService.lambdaQuery()
+                .eq(nonNull(userId), RenewLog::getCreateBy, userId)
+                .page(new Page<>(current,size))
+        );
     }
 
 }
