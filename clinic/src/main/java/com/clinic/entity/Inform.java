fb0abd0c02b686d423493554a60959c54c3e86ba
weimengyao <weimengyao@piesat.cn>
2024-09-06 19:16:07 +0800

    feat：添加通知相关接口

--- clinic/src/main/java/com/clinic/entity/Inform.java
+++ clinic/src/main/java/com/clinic/entity/Inform.java
@@ -1,22 +1,32 @@
 package com.clinic.entity;
 
+import com.clinic.controller.InformController;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Inform implements Serializable {
-
-    private int id;
 
     private String title;
 
     private String content;
 
-    private String time;
+    private Date time;
 
     private int type;
 
     private static final long serialVersionUID = 1L;
 
+    public Inform(InformController.InformVo informVo) {
+        this.title = informVo.getTitle();
+        this.content = informVo.getContent();
+        this.time = new Date();
+        this.type = informVo.getType();
+    }
 }
