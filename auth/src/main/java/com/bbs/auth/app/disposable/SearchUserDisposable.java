a495d52156e93aaea845c158a1c1e46def5fb9dc
luchenlin <2908095738@qq.com>
2024-09-11 16:48:37 +0800

    feat: 增加受邀用户奖励获取相关

--- auth/src/main/java/com/bbs/auth/app/disposable/SearchUserDisposable.java
+++ auth/src/main/java/com/bbs/auth/app/disposable/SearchUserDisposable.java
@@ -4,6 +4,9 @@ import com.bbs.Result;
 import com.bbs.auth.entity.UserDisposable;
 import com.bbs.auth.service.UserDisposableService;
 import com.bbs.auth.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -27,8 +30,16 @@ public class SearchUserDisposable {
         );
     }
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class FinishedParam {
+
+        private String code;
+    }
+
     @PutMapping("/disposable")
-    public Result<Boolean> finished(@RequestParam String code) {
-        return Result.success(userDisposableService.save(new UserDisposable(userService.loginUser().getId(), code)));
+    public Result<Boolean> finished(@RequestBody FinishedParam param) {
+        return Result.success(userDisposableService.save(new UserDisposable(userService.loginUser().getId(), param.getCode())));
     }
 }
